f3da62d1f87cf7e5fc39ffdb31d145c67109f18d
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-08-16 21:00:01 -0300

    feat: Create connection with firestore database.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,90 @@
 import { GetServerSideProps } from "next";
+import { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./styles.module.css";
 import Head from "next/head";
 
 import { getSession } from "next-auth/react";
+import { TextArea } from "@/components/textarea";
+
+import { FiShare2 } from 'react-icons/fi';
+import { FaTrash } from 'react-icons/fa';
 
 export default function Dashboard() {
+  const [input, setInput] = useState("");
+  const [publicTask, setPublicTask] = useState(false);
+
+  function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
+    setPublicTask(event.target.checked);
+  }
+
+  function handleRegisterTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (input === "") return;
+
+    alert("TESTE");
+  }
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Meu painel de tarefas</title>
       </Head>
 
-      <h1>Pagina Painel</h1>
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+            <form>
+              <TextArea
+                placeholder="Digite qual sua tarefa..."
+              />
+              <div className="styles.checkboxArea">
+                <input
+                    type="checkbox"
+                    className={styles.checkbox}
+                    checked={publicTask}
+                    onChange={handleChangePublic}
+                  />
+                <label>Deixa tarefa pública</label>
+              </div>
+
+              <button className={styles.button} type="submit">
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PÚBLICO</label>
+
+              <button className={styles.shareButton}>
+                <FiShare2
+                  size={22}
+                  color="#3183ff"
+                />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa</p>
+              <button className={styles.trashButton}>
+                <FaTrash 
+                  size={24}
+                  color="#ea3140"
+                />
+              </button>
+
+            </div>
+          </article>
+        </section>
+      </main>
     </div>
   );
 }
